d08f2a2de078f3e41a549b77357732cbdd0fda51
yoonjae <realyoon77@gmail.com>
2024-03-08 14:48:56 +0900

    처음2

--- src/main/java/run/Application.java
+++ src/main/java/run/Application.java
@@ -5,5 +5,13 @@ public class Application {
     public static void main(String[] args) {
 
 
+        // main 스위치문 employee == xml ==> 쿼리 , config,common
+        // xml 파일
+        // DTO ==> 필드에 저장해서 불어 //
+        // DAO ==> DAO => 1234 /
+        // 1. create   scn 8개                         //          DTO 지향
+        // 2. read     emo_id  입력하세요 : 전체가읽     //
+        // 3. update   emp_id , 이름 , 민증번호, 급여       //
+        // 4. delete   emp_id 선택 전체 행 삭제            //
     }
 }
