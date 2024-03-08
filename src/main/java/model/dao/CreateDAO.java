f0e52b5ff02edacd33ae7f3a1c3a8324e35ef591
wjdals83 <tjwjdals0803@naver.com>
2024-03-08 18:03:37 +0900

    2024-03-08 프로젝트 작성중

--- src/main/java/model/dao/CreateDAO.java
+++ src/main/java/model/dao/CreateDAO.java
@@ -1,6 +1,62 @@
 package model.dao;
 
+import model.dto.ReadDTO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class CreateDAO {
 
+    private Properties prop = new Properties();
+
+    public CreateDAO() {
+
+        try {
+            prop.loadFromXML((new FileInputStream("src/main/java/mapper/menu-query.xml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public int createNewEmployee(Connection con, ReadDTO newEmployee) {
+
+        PreparedStatement pstmt = null;
+
+        int result = 0;
+
+        String query = prop.getProperty("createEmployee");
+
+        try {
+            pstmt = con.prepareStatement(query);
+
+            pstmt.setString(1, newEmployee.getEmpId());
+            pstmt.setString(2, newEmployee.getEmpName());
+            pstmt.setString(3, newEmployee.getEmpNo());
+            pstmt.setString(4, newEmployee.getEmail());
+            pstmt.setString(5, newEmployee.getPhone());
+            pstmt.setString(6, newEmployee.getDeptCode());
+            pstmt.setString(7, newEmployee.getJobCode());
+            pstmt.setString(8, newEmployee.getSalLevel());
+            pstmt.setInt(9, newEmployee.getSalary());
+            pstmt.setDouble(10, newEmployee.getBonus());
+//            pstmt.setString(11, newEmployee.getManagerId());
+//            pstmt.setString(12, newEmployee.getHireDate());
+//            pstmt.setString(13, newEmployee.getEntDate());
+            pstmt.setString(14, newEmployee.getEntYN());
+
+            result = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+//            close(pstmt);
+        }
+
+        return result;
+    }
 
 }
--- src/main/java/run/Application.java
+++ src/main/java/run/Application.java
@@ -1,5 +1,9 @@
 package run;
 
+import model.dto.ReadDTO;
+
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -13,5 +17,39 @@ public class Application {
         // 2. read     emo_id  입력하세요 : 전체가읽     //
         // 3. update   emp_id , 이름 , 민증번호, 급여       //
         // 4. delete   emp_id 선택 전체 행 삭제            //
+
+        // 신규 사원 등록
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("등록할 사원번호(ID)를 입력해주세요 : ");
+        String empId = sc.nextLine();
+        System.out.print("등록할 사원 이름을 입력해주세요 : ");
+        String empName = sc.nextLine();
+        System.out.print("등록할 주민등록번호를 입력해주세요(ex. 123456-7891234) : ");
+        String empNo = sc.nextLine();
+        System.out.print("등록할 e-mail을 입력해주세요 : ");
+        String email = sc.nextLine();
+        System.out.print("등록할 전화번호를 입력해주세요 : ");
+        String phone = sc.nextLine();
+        System.out.print("등록할 부서코드를 입력해주세요(ex. D9 : ");
+        String deptCode = sc.nextLine();
+        System.out.print("등록할 직업코드를 입력해주세요(ex. J1 : ");
+        String jobCode = sc.nextLine();
+        System.out.print("등록할 급여수준을 입력해주세요(ex. S1 : ");
+        String salLevel = sc.nextLine();
+        System.out.print("등록할 급여를 입력해주세요 : ");
+        int salary = sc.nextInt();
+        System.out.print("등록할 보너스를 입력해주세요(ex. 0.3) : ");
+        double bonus = sc.nextDouble();
+        System.out.print("등록할 관리자Id를 입력해주세요( ex. 200) : ");
+        String managerId = sc.nextLine();
+        System.out.print("등록할 입사일을 입력해주세요 : ");
+        String hireDate = sc.nextLine();
+        System.out.print("등록할 가입일을 입력해주세요 : ");
+        String entDate = sc.nextLine();
+        System.out.print("등록할 입사여부를 입력해주세요('Y' 또는 'N') : ");
+        String entYN = sc.nextLine();
     }
+//    ReadDTO newEmployee = new ReadDTO(empId, empName, empNo, email, phone, deptCode, jobCode, salLevel, salary, bonus, managerId, hireDate, entDate, entYN);
 }
